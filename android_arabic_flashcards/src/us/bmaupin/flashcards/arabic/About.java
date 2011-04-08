2d54fadd6321796b30c07bab10e07c659e77aeae
bmaupin <bmaupin@d45c4bbb-d1aa-4eb0-a131-1cfa29d7c7c7>
2011-04-08 19:50:03 +0000

    make the about screen a little fancier by adding the version name
    git-svn-id: file:///home/bmaupin/tmp/oldaaf/android_arabic_flashcards/trunk@160 d45c4bbb-d1aa-4eb0-a131-1cfa29d7c7c7

--- android_arabic_flashcards/src/us/bmaupin/flashcards/arabic/About.java
+++ android_arabic_flashcards/src/us/bmaupin/flashcards/arabic/About.java
@@ -3,13 +3,30 @@ package us.bmaupin.flashcards.arabic;
 // $Id$
 
 import android.app.Activity;
+import android.content.pm.PackageInfo;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class About extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+		String versionName;
+		
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.about);
-	}
 
+		// get the version name of the app
+		try {
+			PackageInfo packageInfo = getPackageManager().getPackageInfo(getPackageName(), 0);
+			versionName = packageInfo.versionName; 
+
+		} catch (android.content.pm.PackageManager.NameNotFoundException e) {
+		    e.printStackTrace();
+		    versionName = "Cannot load Version!";
+		}
+		
+		TextView tv = (TextView)findViewById(R.id.about_content);
+		
+		tv.setText("Version: " + versionName + "\n\n" + getString(R.string.about_text));
+	}
 }
