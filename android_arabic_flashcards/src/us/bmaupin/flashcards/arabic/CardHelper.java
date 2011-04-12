d0bd6aebf84e1320201d4c1396fbf18aff013625
bmaupin <bmaupin@d45c4bbb-d1aa-4eb0-a131-1cfa29d7c7c7>
2011-04-12 18:51:44 +0000

    remove unused imports
    git-svn-id: file:///home/bmaupin/tmp/oldaaf/android_arabic_flashcards/trunk@167 d45c4bbb-d1aa-4eb0-a131-1cfa29d7c7c7

--- android_arabic_flashcards/src/us/bmaupin/flashcards/arabic/CardHelper.java
+++ android_arabic_flashcards/src/us/bmaupin/flashcards/arabic/CardHelper.java
@@ -3,15 +3,9 @@ package us.bmaupin.flashcards.arabic;
 // $Id$
 
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.HashMap;
-import java.util.Iterator;
-import java.util.LinkedHashMap;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
-import java.util.Random;
 
 import android.content.ContentValues;
 import android.content.Context;
