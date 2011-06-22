f7c91092f088254507d4d89df2af5c7dda68315d
bmaupin <bmaupin@d45c4bbb-d1aa-4eb0-a131-1cfa29d7c7c7>
2011-06-22 20:22:52 +0000

    working on getting settings menu and preferences to work with custom preferences file based on profile name
    git-svn-id: file:///home/bmaupin/tmp/oldaaf/android_arabic_flashcards/trunk@232 d45c4bbb-d1aa-4eb0-a131-1cfa29d7c7c7

--- android_arabic_flashcards/src/us/bmaupin/flashcards/arabic/Settings.java
+++ android_arabic_flashcards/src/us/bmaupin/flashcards/arabic/Settings.java
@@ -2,20 +2,58 @@ package us.bmaupin.flashcards.arabic;
 
 // $Id$
 
-import android.content.Context;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
-import android.preference.PreferenceManager;
 
 public class Settings extends PreferenceActivity {
+    private String profileName = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        
+        if (savedInstanceState != null) {
+            // get the profile name from the intent's bundle
+            profileName = savedInstanceState.getString(ArabicFlashcards.EXTRA_PROFILE_NAME);
+        }
+        
+        // set the preferences file to one based on the profile name
+        getPreferenceManager().setSharedPreferencesName(profileName);
+        // get the default settings from XML
         addPreferencesFromResource(R.xml.settings);
     }
     
+/*    
+    public static String getDefaultLang(Context context, String profileName) {
+        return PreferenceManager.getDefaultSharedPreferences(context)
+                .getString("defaultLang", "arabic");
+    }
+    
+    public static boolean getAskCardOrder(Context context, String profileName) {
+        return PreferenceManager.getDefaultSharedPreferences(context)
+                .getBoolean("askCardOrder", false);
+    }
+*/
+    
+/*    
+    void setProfile(String profileName) {
+        pm.setSharedPreferencesName(profileName);
+    }
+*/
+    
+/*
+    public String getDefaultLang(Context context, profileNmae) {
+        return pm.getSharedPreferences().getString("defaultLang", "arabic");
+    }
+
+    public boolean getAskCardOrder(Context context) {
+        return pm.getSharedPreferences().getBoolean("askCardOrder", false);
+    }
+*/
+    
+/*
     public static String getDefaultLang(Context context) {
+        return pm.getSharedPreferences().getString("defaultLang", "arabic");
         return PreferenceManager.getDefaultSharedPreferences(context)
                 .getString("defaultLang", "arabic");
     }
@@ -24,4 +62,5 @@ public class Settings extends PreferenceActivity {
         return PreferenceManager.getDefaultSharedPreferences(context)
                 .getBoolean("askCardOrder", false);
     }
+*/
 }
