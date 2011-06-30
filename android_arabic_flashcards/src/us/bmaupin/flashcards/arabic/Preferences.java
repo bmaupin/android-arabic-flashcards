0118642734eb50b5de09918f2685135fa2cc4f60
bmaupin <bmaupin@d45c4bbb-d1aa-4eb0-a131-1cfa29d7c7c7>
2011-06-30 19:30:45 +0000

    disable default card order preference in preference activity if ask card order is checked.  also put preference keys in xml.  some more testing probably needs to be done.
    git-svn-id: file:///home/bmaupin/tmp/oldaaf/android_arabic_flashcards/trunk@241 d45c4bbb-d1aa-4eb0-a131-1cfa29d7c7c7

--- android_arabic_flashcards/src/us/bmaupin/flashcards/arabic/Preferences.java
+++ android_arabic_flashcards/src/us/bmaupin/flashcards/arabic/Preferences.java
@@ -2,14 +2,20 @@ package us.bmaupin.flashcards.arabic;
 
 // $Id$
 
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.os.Bundle;
+import android.preference.CheckBoxPreference;
+import android.preference.Preference;
 import android.preference.PreferenceActivity;
+import android.util.Log;
 
-public class Preferences extends PreferenceActivity {
+public class Preferences extends PreferenceActivity implements OnSharedPreferenceChangeListener {
     private String profileName = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d("Preferences", "onCreate called");
         super.onCreate(savedInstanceState);
         
         // get the profile name from the extra data in the intent
@@ -18,5 +24,20 @@ public class Preferences extends PreferenceActivity {
         getPreferenceManager().setSharedPreferencesName(profileName);
         // get the default preferences from XML
         addPreferencesFromResource(R.xml.preferences);
+        // set up a listener whenever a key changes            
+        getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
     }
+    
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        // when the ask card order preference value changes
+        if (key.equals(getString(R.string.preferences_ask_card_order))) {
+            CheckBoxPreference askCardOrder = (CheckBoxPreference)
+                findPreference(getString(R.string.preferences_ask_card_order));
+            Preference defaultCardOrder = 
+                findPreference(getString(R.string.preferences_default_card_order));
+            // disable default card order preference if ask card order is checked
+            defaultCardOrder.setEnabled(!askCardOrder.isChecked());
+        }
+    }
+
 }
