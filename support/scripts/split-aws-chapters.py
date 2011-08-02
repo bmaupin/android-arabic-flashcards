8e0974cea6e3192eacd399f05af50d2d3c1e7e8e
bmaupin <bmaupin@d45c4bbb-d1aa-4eb0-a131-1cfa29d7c7c7>
2011-08-02 18:50:22 +0000

    commit working version of script to split out aws chapters into separate table
    git-svn-id: file:///home/bmaupin/tmp/oldaaf/android_arabic_flashcards/trunk@262 d45c4bbb-d1aa-4eb0-a131-1cfa29d7c7c7

--- support/scripts/split-aws-chapters.py
+++ support/scripts/split-aws-chapters.py
@@ -15,28 +15,48 @@ def main():
             return True
         except ValueError:
             return False
-
+    
+    def correct_id(old_id):
+#        if len(dupes) < 1:
+#            return old_id
+        count = 0
+        for dupe_id in dupes:
+            if old_id < dupe_id:
+                return old_id - count
+            count += 1
+#        return old_id - len(dupes)
+                
+            
+    
+    current_id = 1
+#    dupe_count = 0
+    dupes = []
+    # chapter, card id
+    aws_cards = []
     
     infile = open(infile_name)
     new_words = open(new_words_name, 'w')
-    
-    count = 1
-    dupe_count = 0
-    # chapter, card id
-    aws_cards = []
     
     for line in infile:
         values = line.strip().split('|')
         # make sure there's a value for the aws chapter
         if values[6] != '':
+            # if it's a duplicate
             if represents_int(values[0]) and values[1] == '':
-                aws_cards.append([values[6], int(values[0]) - dupe_count])
-                dupe_count += 1
+                dupes.append(current_id)
+                aws_cards.append([values[6], correct_id(int(values[0]))])
+#                aws_cards.append([values[6], int(values[0]) - dupe_count])
+#                dupe_count += 1
             else:
-                aws_cards.append([values[6], count - dupe_count])
+                aws_cards.append([values[6], current_id - len(dupes)])
                 new_words.write(line)
-        count += 1
-    
+#                aws_cards.append([values[6], count - dupe_count])
+#                new_words.write(line)
+        # we need to write the line even if there's no aws chapter value
+        else:
+            new_words.write(line)
+        current_id += 1
+        
     new_words.close()
     
     aws_chapters = open(aws_chapters_name, 'w')
@@ -44,6 +64,10 @@ def main():
         aws_chapters.write('%s|%s\n' % (item[0], item[1]))
     
     aws_chapters.close()
+    
+    dupes.sort()
+    print '%s dupes:' % (len(dupes))
+    print dupes
         
         
 #        line.find('|')
