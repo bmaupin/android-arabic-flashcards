26d072753f6987d38e1b30934efdee88908eb57d
bmaupin <bmaupin@d45c4bbb-d1aa-4eb0-a131-1cfa29d7c7c7>
2011-08-17 18:23:22 +0000

    put vowels back in database for testing arabic shaper class
    git-svn-id: file:///home/bmaupin/tmp/oldaaf/android_arabic_flashcards/trunk@285 d45c4bbb-d1aa-4eb0-a131-1cfa29d7c7c7

--- android_arabic_flashcards/src/us/bmaupin/flashcards/arabic/DatabaseHelper.java
+++ android_arabic_flashcards/src/us/bmaupin/flashcards/arabic/DatabaseHelper.java
@@ -18,8 +18,8 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 	public static final String TAG = "DatabaseHelper";
 	
 	public static final String DATABASE_NAME = "words.db";
-	public static final int DATABASE_VERSION = 7;
-	public static final String WORDS_TABLE = "words";
+	public static final int DATABASE_VERSION = 8;
+	public static final String WORDS_TABLE = "vowels";
 	public static final String AWS_CHAPTERS_TABLE = "aws_chapters";
 
 	// The name of each column in the database
