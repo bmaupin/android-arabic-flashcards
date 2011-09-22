2137bb9e00952052ff5f841026aaacd592c31f39
bmaupin <bmaupin@d45c4bbb-d1aa-4eb0-a131-1cfa29d7c7c7>
2011-09-22 19:45:40 +0000

    more work on search
    git-svn-id: file:///home/bmaupin/tmp/oldaaf/android_arabic_flashcards/trunk@323 d45c4bbb-d1aa-4eb0-a131-1cfa29d7c7c7

--- android_arabic_flashcards/src/us/bmaupin/flashcards/arabic/Search.java
+++ android_arabic_flashcards/src/us/bmaupin/flashcards/arabic/Search.java
@@ -2,19 +2,23 @@ package us.bmaupin.flashcards.arabic;
 
 // $Id$
 
-import android.app.Activity;
+import android.app.ListActivity;
 import android.app.SearchManager;
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.util.Log;
 
-public class Search extends Activity {
+public class Search extends ListActivity {
     private static final String TAG = "Search";
+    
+    private SQLiteDatabase db;
+    private DatabaseHelper dbHelper;
     
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        setContentView(R.layout.search);
+        setContentView(R.layout.search);
 
         // Get the intent, verify the action and get the query
         Intent intent = getIntent();
@@ -22,9 +26,20 @@ public class Search extends Activity {
           String query = intent.getStringExtra(SearchManager.QUERY);
           doSearch(query);
         }
+        
+        dbHelper = new DatabaseHelper(this);
+        db = dbHelper.getReadableDatabase();
     }
     
     void doSearch(String query) {
         Log.d(TAG, "query=" + query);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        
+        db.close();
+        dbHelper.close();
     }
 }
