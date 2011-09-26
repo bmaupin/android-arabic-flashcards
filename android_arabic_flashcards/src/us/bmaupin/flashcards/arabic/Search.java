7345c82fb0075369b3a7e590302036929cf73ae6
bmaupin <bmaupin@d45c4bbb-d1aa-4eb0-a131-1cfa29d7c7c7>
2011-09-26 19:15:14 +0000

    more work on search.  possible bug: _id column when using simplecursoradapter seems to be case sensitive?
    git-svn-id: file:///home/bmaupin/tmp/oldaaf/android_arabic_flashcards/trunk@324 d45c4bbb-d1aa-4eb0-a131-1cfa29d7c7c7

--- android_arabic_flashcards/src/us/bmaupin/flashcards/arabic/Search.java
+++ android_arabic_flashcards/src/us/bmaupin/flashcards/arabic/Search.java
@@ -5,9 +5,12 @@ package us.bmaupin.flashcards.arabic;
 import android.app.ListActivity;
 import android.app.SearchManager;
 import android.content.Intent;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ListAdapter;
+import android.widget.SimpleCursorAdapter;
 
 public class Search extends ListActivity {
     private static final String TAG = "Search";
@@ -17,18 +20,38 @@ public class Search extends ListActivity {
     
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        String query = "";
+        
         super.onCreate(savedInstanceState);
         setContentView(R.layout.search);
 
         // Get the intent, verify the action and get the query
         Intent intent = getIntent();
         if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
-          String query = intent.getStringExtra(SearchManager.QUERY);
+          query = intent.getStringExtra(SearchManager.QUERY);
           doSearch(query);
         }
         
         dbHelper = new DatabaseHelper(this);
         db = dbHelper.getReadableDatabase();
+        
+        String[] columns = new String[] {DatabaseHelper._ID, 
+                DatabaseHelper.WORDS_ENGLISH};
+        String selection = DatabaseHelper.WORDS_ENGLISH + " LIKE ?";
+        String[] selectionArgs = new String[] {"%" + query + "%"};
+        
+        Cursor cursor = db.query(DatabaseHelper.WORDS_TABLE, columns, selection, selectionArgs, null, null, null);
+        startManagingCursor(cursor);
+        
+        ListAdapter adapter = new SimpleCursorAdapter(
+                this,
+                android.R.layout.simple_list_item_1,
+                cursor,
+                new String[] {DatabaseHelper.WORDS_ENGLISH},
+                new int[] {android.R.id.list});
+
+        // Bind to our new adapter.
+        setListAdapter(adapter);
     }
     
     void doSearch(String query) {
