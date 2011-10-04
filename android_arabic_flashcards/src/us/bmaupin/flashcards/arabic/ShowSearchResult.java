808a308bfa32628e6ce30d14624b9a4dce404639
bmaupin <bmaupin@d45c4bbb-d1aa-4eb0-a131-1cfa29d7c7c7>
2011-10-04 19:18:22 +0000

    still working on showing search results
    git-svn-id: file:///home/bmaupin/tmp/oldaaf/android_arabic_flashcards/trunk@337 d45c4bbb-d1aa-4eb0-a131-1cfa29d7c7c7

--- android_arabic_flashcards/src/us/bmaupin/flashcards/arabic/ShowSearchResult.java
+++ android_arabic_flashcards/src/us/bmaupin/flashcards/arabic/ShowSearchResult.java
@@ -2,24 +2,37 @@ package us.bmaupin.flashcards.arabic;
 
 //$Id$
 
+import java.util.Map;
+
 import android.app.Activity;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class ShowSearchResult extends Activity {
 //    private static final String TAG = "ShowSearchResult";
     
+    private String arabic = "";
     private int cardId;
     private SQLiteDatabase db;
     private DatabaseHelper dbHelper;
+    private String english = "";
+    private String language = "";
+    private TextView tv;
     
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         
         cardId = getIntent().getExtras().getInt(Search.EXTRA_CARD_ID);
+        
+        setContentView(R.layout.search_result);
     }
 
     @Override
@@ -39,11 +52,40 @@ public class ShowSearchResult extends Activity {
         startManagingCursor(cursor);
         
         cursor.moveToFirst();
-        String english = cursor.getString(0);
-        String arabic = cursor.getString(1);
+        english = cursor.getString(0);
+        arabic = cursor.getString(1);
         
         Toast.makeText(this, "english: " + english + "\tarabic: " + arabic, Toast.LENGTH_LONG)
         .show();
+        
+        tv = (TextView) findViewById(R.id.textview);
+        tv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                flipCard();
+            }
+        });
+        
+        flipCard();
+    }
+    
+    /**
+     * Given a view, a word, and a language, shows the word in the view and 
+     * formats it depending on the language
+     */
+    private void flipCard() {
+/* TODO: for now, show arabic first
+ * in the future, show whichever language wasn't searched for
+ */
+        if (language.equals("english") || language.equals("")) {
+            tv.setTextSize(56f);
+            language = "arabic";
+            tv.setText(HelperMethods.fixArabic(arabic));
+        } else if (language.equals("arabic")) {
+            tv.setTextSize(42f);
+            language = "english";
+            tv.setText(english);
+        }
     }
 
     @Override
