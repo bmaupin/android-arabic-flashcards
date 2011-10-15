e4d7f8ecb26294ffd7822a7a07d7e8d3be1ef7d7
Bryan Maupin <bmaupincode@gmail.com>
2011-10-15 18:20:46 -0500

    replace study set db helper code with simple db helper code.  we'll add to it as necessary

--- android_arabic_flashcards/src/us/bmaupin/flashcards/arabic/StudySetDatabaseHelper.java
+++ android_arabic_flashcards/src/us/bmaupin/flashcards/arabic/StudySetDatabaseHelper.java
@@ -1,90 +1,57 @@
 package us.bmaupin.flashcards.arabic;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import android.content.Context;
-import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.provider.BaseColumns;
 import android.util.Log;
 
 public class StudySetDatabaseHelper extends SQLiteOpenHelper {
-	// tag for log messages
-    private static final String TAG = "StudySetDatabaseHelper";
+    // The tag will be used for logging
+    public static final String TAG = "StudySetDatabaseHelper";
 
     // The name of your database
-    public static final String DATABASE_NAME = "profiles.db";
+    public static final String DATABASE_NAME = "studysets.db";
     // The version of your database (increment this every time you change something)
     public static final int DATABASE_VERSION = 1;
-    // profile name; this will be used as the database table name
-    private static final String DEFAULT_STUDY_SET_TABLE_NAME = "profile1";
-    private String profileTableName = "";
-    
+    // The name of the table in your database
+    public static final String META_TABLE_NAME = "studysets_meta";
+   
     // The name of each column in the database
     public static final String _ID = BaseColumns._ID;
-    public static final String CARD_ID = "card_ID";
-    public static final String STATUS = "status";
-    
-    // SQL Statement to create a new database.
-    private final String DB_TABLE_CREATE =
-        "CREATE TABLE %s (" +
-        _ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
-        CARD_ID + " INTEGER UNIQUE, " +
-        STATUS + " INTEGER);";
+    public static final String SET_ID = "set_id";
+    public static final String SET_NAME = "set_name";
 
+    // SQL Statement to create a new database.
+    public static final String META_TABLE_CREATE =
+        "CREATE TABLE " + META_TABLE_NAME + " (" +
+        _ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+        SET_ID + " TEXT, " +
+        SET_NAME + " TEXT);";
+   
     // The constructor method
     public StudySetDatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
-    
-    void setStudySetTableName(String profileTableName) {
-        if (profileTableName.equals("")) {
-            this.profileTableName = DEFAULT_STUDY_SET_TABLE_NAME;
-        } else {
-            this.profileTableName = profileTableName;
-        }
-        verifyStudySetTableExists(this.profileTableName);
-    }
-    
-    public String getStudySetTableName() {
-        return profileTableName;
-    }
-    
-    private void verifyStudySetTableExists(String profileTableName) {
-        List<String> tables = new ArrayList<String>();
-        final String SQL_GET_ALL_TABLES = "SELECT name FROM " + 
-            "sqlite_master WHERE type='table' ORDER BY name";
-        
-        // fetch the database
-        SQLiteDatabase db = super.getReadableDatabase();
-        // get the list of tables in the db
-        Cursor cursor = db.rawQuery(SQL_GET_ALL_TABLES, null);
-        while (cursor.moveToNext()) {
-            tables.add(cursor.getString(0));
-        }
-        cursor.close();
-        // if the table we want isn't in the db, create it
-        if (!tables.contains(profileTableName)) {
-            db.execSQL(String.format(DB_TABLE_CREATE, profileTableName));
-        }
-        
-        db.close();
-    }
-    
+
     /* Called when the super class getWritableDatabase (or getReadableDatabase)
      * method determines that the database doesn't exist yet and needs to be created
      */
     @Override
     public void onCreate(SQLiteDatabase db) {
-        // don't do anything here because we'll create the profile tables manually
-//
-        Log.d(TAG, "onCreate called");
+        db.execSQL(META_TABLE_CREATE);       
     }
 
-	@Override
-	public void onUpgrade(SQLiteDatabase arg0, int arg1, int arg2) {
-		// nothing to do here
-	}
+    /* Called when the super class getWritableDatabase (or getReadableDatabase)
+     * method determines that the existing database isn't the same version as
+     * the latest version
+     */
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        Log.w(TAG, "Upgrading database from version " + oldVersion + " to "
+                + newVersion + ", which will destroy all old data");
+        // this is probably not the best way to do it...
+//        db.execSQL("DROP TABLE IF EXISTS " + DB_TABLE_NAME);
+//        onCreate(db);
+    }
 }
