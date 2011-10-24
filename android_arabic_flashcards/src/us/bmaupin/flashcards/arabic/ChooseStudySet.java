c49ef54d14fb5c07cebee9d566c9f2b0834bf73a
bmaupin <bmaupin@users.noreply.github.com>
2011-10-24 14:49:26 -0500

    for now, set ChooseStudySet to be the main activity.  we'll need to clean up the android manifest once we get this all sorted.  also add a separate activity for free mode, at least for now.

--- android_arabic_flashcards/src/us/bmaupin/flashcards/arabic/ChooseStudySet.java
+++ android_arabic_flashcards/src/us/bmaupin/flashcards/arabic/ChooseStudySet.java
@@ -1,14 +1,16 @@
 package us.bmaupin.flashcards.arabic;
 
+import android.app.Activity;
 import android.app.ListActivity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
-import android.widget.Toast;
 
 public class ChooseStudySet extends ListActivity {
+    private static final int REQUEST_CARD_SET = 0;
     private static final String TAG = "ChooseStudySet";
     
     @Override
@@ -22,7 +24,8 @@ public class ChooseStudySet extends ListActivity {
         button.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View arg0) {
-                Toast.makeText(getApplicationContext(), "DEBUG: button clicked", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(ChooseStudySet.this, ChooseCardSet.class);
+                startActivityForResult(intent, REQUEST_CARD_SET);
             }
         });
     }
@@ -33,6 +36,44 @@ public class ChooseStudySet extends ListActivity {
         Log.d(TAG, "onResume()");
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        
+        Log.d(TAG, "onActivityResult()");
+        
+        switch(requestCode) {
+            case (REQUEST_CARD_SET) :
+                if (resultCode == Activity.RESULT_OK) {
+                    String cardSet = data.getStringExtra(ChooseCardSet.EXTRA_CARD_SET);
+                    Log.d(TAG, "onActivityResult: cardSet=" + cardSet);
+                    
+                    if (data.getStringExtra(ChooseCardSet.EXTRA_CARD_SUBSET) == null) {
+//                        ch.loadCardSet(cardSet);
+                    } else {
+//                        ch.loadCardSet(cardSet, data.getStringExtra(
+//                                ChooseCardSet.CARD_SUBSET));
+                    }
+                    
+//                    if (ch.isAskCardOrder()) {
+//                        showDialog(DIALOG_SELECT_CARD_ORDER);
+//                    } else {
+//                        showFirstCard();
+//                    }
+                    
+                    Intent intent = new Intent(this, FreeMode.class);
+                    intent.putExtra(ChooseCardSet.EXTRA_CARD_SET, 
+                            data.getStringExtra(ChooseCardSet.EXTRA_CARD_SET));
+                    intent.putExtra(ChooseCardSet.EXTRA_CARD_SUBSET, 
+                            data.getStringExtra(ChooseCardSet.EXTRA_CARD_SUBSET));
+                    startActivity(intent);
+                    
+//                    startActivity(new Intent(this, FreeMode.class));
+                }
+                break;
+        }
+    }
+    
     @Override
     protected void onPause() {
         super.onPause();
