dfd3358a10b2bd33acd894db6a52b7cefe6bf66d
bmaupin <bmaupin@users.noreply.github.com>
2011-10-30 16:17:49 -0500

    working on updating aws chapters when we separate vowels

--- support/scripts/separate-vowels.py
+++ support/scripts/separate-vowels.py
@@ -3,16 +3,41 @@
 '''Use this script to find possible duplicates in one arabic words source file
 '''
 
+import sys
+
 
 infile_name = '../words/arabic-words.csv'
 outfile_name = 'arabic-words-separated-vowels.csv'
+aws_chapters_name = '../words/aws-chapters.csv'
+aws_chapters_new_name = 'aws-chapters-new.csv'
 
 
 def main():
+    def represents_int(s):
+        try:
+            int(s)
+            return True
+        except ValueError:
+            return False
+
+    card_chapters = {}
+    aws_chapters = open(aws_chapters_name)
+
+    for line in aws_chapters:
+        line = line.strip()
+        chapter, cardid = line.split('|')
+        cardid = int(cardid)
+        if cardid not in card_chapters:
+            card_chapters[cardid] = []
+        card_chapters[cardid].append(chapter)
+
+    aws_chapters.close()
+
     infile = open(infile_name)
     outfile = open(outfile_name, 'w')
     
     index = 0
+    plurals = 0
     for line in infile:
         index += 1
         line = line.strip()
@@ -21,30 +46,71 @@ def main():
         if plural == '':
             outfile.write(line + '|n\n')
         else:
+#            plurals += 1
             # write out the singular line
             outfile.write(('|').join([english, arabic, part, category, gender]) + '|n\n')
 
-            #english_plural = make_english_plural(english)
-            english_plural = []
-            joined_commas = []
-            joined_semicolons = []
-            for split_commas in english.split(','):
-                if split_commas.find(';') == -1:
-                    split_commas = make_english_plural(split_commas)
-                    #joined_commas.append(split_commas)
-                    english_plural.append(split_commas)
-                else:
-                    for split_semicolons in split_commas.split(';'):
-                        split_semicolons = make_english_plural(split_semicolons)
-                        joined_semicolons.append(split_semicolons)
-                    english_plural.append(';'.join(joined_semicolons))
+            for cardid in sorted(card_chapters, reverse=True):
+                if cardid > index + plurals:
+                    card_chapters[cardid + 1] = card_chapters[cardid]
+                    
+                try:
+                    card_chapters[index + plurals + 1] = card_chapters[index + plurals]
+                    break
+                except KeyError:
+                    print 'index: ' + str(index)
+                    print 'plurals: ' + str(plurals)
+                    print 'cardid: ' + str(cardid)
+                    print 'card_chapters[cardid]: ' + str(card_chapters[cardid])
+                    print 'card_chapters[cardid + 1]: ' + str(card_chapters[cardid + 1])
+                    print 'card_chapters[index + plurals + 1]: ' + str(card_chapters[index + plurals + 1])
+                    print 'card_chapters[index + plurals]: ' + str(card_chapters[index + plurals])
+                    sys.exit()
+                
+#            card_chapters[index + plurals + 1] = 'test'
 
-            english_plural = ','.join(english_plural)
+            if part != 'noun':
+                english_plural = english + ' (pl)'
+            else:
+                #english_plural = make_english_plural(english)
+                english_plural = []
+                joined_commas = []
+                joined_semicolons = []
+                for split_commas in english.split(','):
+                    if split_commas.find(';') == -1:
+                        split_commas = make_english_plural(split_commas)
+                        #joined_commas.append(split_commas)
+                        english_plural.append(split_commas)
+                    else:
+                        for split_semicolons in split_commas.split(';'):
+                            split_semicolons = make_english_plural(split_semicolons)
+                            joined_semicolons.append(split_semicolons)
+                        english_plural.append(';'.join(joined_semicolons))
+
+                english_plural = ','.join(english_plural)
             # write out the plural line
             outfile.write(('|').join([english_plural, plural, part, category, gender]) + '|y\n')
 
+            plurals += 1
+
     outfile.close()
     infile.close()
+
+    chapters = []
+    for cardid in card_chapters:
+        for chapter in card_chapters[cardid]:
+            if chapter not in chapters:
+                chapters[chapter] = []
+            chapters[chapter].append(cardid)
+
+    aws_chapters_new = open(aws_chapters_new_name, 'w')
+
+    for chapter in chapters:
+        for cardid in sorted(chapters[cardid]):
+            aws_chapters_new.write('%s|%s\n' % (chapter, cardid))
+
+    aws_chapters_new.close()
+
 
 def make_english_plural(english):
     non_plurals = ['news',
