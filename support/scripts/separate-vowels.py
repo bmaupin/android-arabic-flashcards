9e5a075c7eb027641772e6b06f10f0479d0390ff
bmaupin <bmaupin@users.noreply.github.com>
2011-10-30 00:03:42 -0500

    update script to handle words separated by commas, semicolons, and a few other cases

--- support/scripts/separate-vowels.py
+++ support/scripts/separate-vowels.py
@@ -15,18 +15,33 @@ def main():
     index = 0
     for line in infile:
         index += 1
-#        line = line.strip()
+        line = line.strip()
         english, arabic, plural, part, category, gender = line.split('|')
 
         if plural == '':
-            outfile.write(line)
+            outfile.write(line + '|n\n')
         else:
             # write out the singular line
-            outfile.write(('|').join([english, arabic, part, category, gender]))
+            outfile.write(('|').join([english, arabic, part, category, gender]) + '|n\n')
 
-            english_plural = make_english_plural(english)
+            #english_plural = make_english_plural(english)
+            english_plural = []
+            joined_commas = []
+            joined_semicolons = []
+            for split_commas in english.split(','):
+                if split_commas.find(';') == -1:
+                    split_commas = make_english_plural(split_commas)
+                    #joined_commas.append(split_commas)
+                    english_plural.append(split_commas)
+                else:
+                    for split_semicolons in split_commas.split(';'):
+                        split_semicolons = make_english_plural(split_semicolons)
+                        joined_semicolons.append(split_semicolons)
+                    english_plural.append(';'.join(joined_semicolons))
+
+            english_plural = ','.join(english_plural)
             # write out the plural line
-            outfile.write(('|').join([english_plural, plural, part, category, gender]))
+            outfile.write(('|').join([english_plural, plural, part, category, gender]) + '|y\n')
 
     outfile.close()
     infile.close()
@@ -55,17 +70,17 @@ def make_english_plural(english):
                        'man': 'men',
                        'child': 'children',
                       }
-
+    
     for word in special_plurals:
         if english.find(word) != -1:
             return english.replace(word, special_plurals[word]
     '''
-
+    
     if english in non_plurals:
         return '%s (pl)' % (english)
-    elif english.endswith('y'):
+    elif english.endswith('y') and english[-2] != 'o' and english[-2] != 'a':
         return english[:-1] + 'ies'
-    elif english.endswith('s'):
+    elif english.endswith('s') or english.endswith('sh') or english.endswith('ch'):
         return english + 'es'
     else:
         return english + 's'
