2bf5c9aca3ef31fd2ab816f4c4f1c8bd2b099e20
bmaupin <bmaupin@users.noreply.github.com>
2011-11-13 17:11:18 -0600

    clicking on the create new study set button asks the user to pick a card set and then brings up a create study set dialog

--- android_arabic_flashcards/src/us/bmaupin/flashcards/arabic/ChooseStudySet.java
+++ android_arabic_flashcards/src/us/bmaupin/flashcards/arabic/ChooseStudySet.java
@@ -1,19 +1,27 @@
 package us.bmaupin.flashcards.arabic;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.app.ListActivity;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class ChooseStudySet extends ListActivity {
-    private static final int REQUEST_CARD_SET = 0;
+	private static final int DIALOG_CREATE_STUDY_SET = 0;
+	private static final int REQUEST_CARD_SET_BROWSE = 0;
+    private static final int REQUEST_CARD_SET_CREATE = 1;
     private static final String TAG = "ChooseStudySet";
     
     @Override
@@ -23,13 +31,23 @@ public class ChooseStudySet extends ListActivity {
         
         setContentView(R.layout.choose_study_set);
         
-        Button button = (Button)findViewById(
+        Button browseCardsButton = (Button)findViewById(
         		R.id.study_set_button_browse_cards);
-        button.setOnClickListener(new OnClickListener() {
+        browseCardsButton.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View arg0) {
                 Intent intent = new Intent(ChooseStudySet.this, ChooseCardSet.class);
-                startActivityForResult(intent, REQUEST_CARD_SET);
+                startActivityForResult(intent, REQUEST_CARD_SET_BROWSE);
+            }
+        });
+        
+        Button createNewButton = (Button)findViewById(
+        		R.id.study_set_button_create_new);
+        createNewButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                Intent intent = new Intent(ChooseStudySet.this, ChooseCardSet.class);
+                startActivityForResult(intent, REQUEST_CARD_SET_CREATE);
             }
         });
     }
@@ -47,17 +65,19 @@ public class ChooseStudySet extends ListActivity {
         Log.d(TAG, "onActivityResult()");
         
         switch(requestCode) {
-            case (REQUEST_CARD_SET) :
+            case (REQUEST_CARD_SET_BROWSE) :
                 if (resultCode == Activity.RESULT_OK) {
                     String cardSet = data.getStringExtra(ChooseCardSet.EXTRA_CARD_SET);
                     Log.d(TAG, "onActivityResult: cardSet=" + cardSet);
                     
+/*                    
                     if (data.getStringExtra(ChooseCardSet.EXTRA_CARD_SUBSET) == null) {
 //                        ch.loadCardSet(cardSet);
                     } else {
 //                        ch.loadCardSet(cardSet, data.getStringExtra(
 //                                ChooseCardSet.CARD_SUBSET));
                     }
+*/
                     
 //                    if (ch.isAskCardOrder()) {
 //                        showDialog(DIALOG_SELECT_CARD_ORDER);
@@ -73,6 +93,22 @@ public class ChooseStudySet extends ListActivity {
                     startActivity(intent);
                     
 //                    startActivity(new Intent(this, FreeMode.class));
+                }
+            	break;
+            case (REQUEST_CARD_SET_CREATE) :
+                if (resultCode == Activity.RESULT_OK) {
+                    String cardSet = data.getStringExtra(ChooseCardSet.EXTRA_CARD_SET);
+                    Log.d(TAG, "onActivityResult: cardSet=" + cardSet);
+                    
+/*                    
+                    Intent intent = new Intent(this, BrowseCards.class);
+                    intent.putExtra(ChooseCardSet.EXTRA_CARD_SET, 
+                            data.getStringExtra(ChooseCardSet.EXTRA_CARD_SET));
+                    intent.putExtra(ChooseCardSet.EXTRA_CARD_SUBSET, 
+                            data.getStringExtra(ChooseCardSet.EXTRA_CARD_SUBSET));
+                    startActivity(intent);
+*/
+                    showDialog(DIALOG_CREATE_STUDY_SET);
                 }
                 break;
         }
@@ -110,6 +146,59 @@ public class ChooseStudySet extends ListActivity {
     }
     
     @Override
+	protected Dialog onCreateDialog(int id) {
+        switch (id) {
+	        case DIALOG_CREATE_STUDY_SET:
+	            return createCreateStudySetDialog();
+        }
+        return null;
+	}
+    
+    private Dialog createCreateStudySetDialog() {
+        final CharSequence[] items = {"Red", "Green", "Blue"};
+
+        View layout = LayoutInflater.from(this).inflate(
+        		R.layout.dialog_create_study_set, 
+        		(ViewGroup) findViewById(R.id.dialog_create_study_set_layout));
+        
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setView(layout)
+               .setCancelable(true)
+        	   .setTitle(getString(R.string.dialog_create_study_set_title))
+               .setPositiveButton(getString(
+            		   R.string.dialog_create_study_set_positive_button), 
+            		   new DialogInterface.OnClickListener() {
+		           public void onClick(DialogInterface dialog, int id) {
+//		                MyActivity.this.finish();
+		           }
+               })
+	           .setNegativeButton(getString(
+	        		   R.string.dialog_create_study_set_negative_button), 
+	        		   new DialogInterface.OnClickListener() {
+	               public void onClick(DialogInterface dialog, int id) {
+	                    dialog.cancel();
+	               }
+	           });
+        
+        
+/*
+        builder.setItems(items, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int item) {
+                Toast.makeText(getApplicationContext(), items[item], Toast.LENGTH_SHORT).show();
+            }
+        });
+        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                 MyActivity.this.finish();
+            }
+        })
+*/
+
+        AlertDialog ad = builder.create();
+        return ad;
+    }
+
+	@Override
     protected void onPause() {
         super.onPause();
         Log.d(TAG, "onPause()");
