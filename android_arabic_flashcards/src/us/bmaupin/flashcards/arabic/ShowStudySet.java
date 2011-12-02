5386436ca495e784f6784799cb6363d0861f42dd
bmaupin <bmaupin@users.noreply.github.com>
2011-12-02 13:48:35 -0600

    add menu

--- android_arabic_flashcards/src/us/bmaupin/flashcards/arabic/ShowStudySet.java
+++ android_arabic_flashcards/src/us/bmaupin/flashcards/arabic/ShowStudySet.java
@@ -1,8 +1,12 @@
 package us.bmaupin.flashcards.arabic;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class ShowStudySet extends Activity {
     private static final String TAG = "MyActivity";
@@ -17,6 +21,31 @@ public class ShowStudySet extends Activity {
     protected void onResume() {
         super.onResume();
         Log.d(TAG, "onResume()");
+    }
+    
+    /* Inflates the menu */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+    
+    /* Handles menu selections */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_help:
+                startActivity(new Intent(this, Help.class));
+                return true;
+            case R.id.menu_settings:
+                startActivity(new Intent(this, Preferences.class));
+                return true;
+            case R.id.menu_search:
+                onSearchRequested();
+                return true;
+        }
+        return false;
     }
 
     @Override
