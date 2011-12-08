8cd5a003dc7281d6bbb4e4cce7448cd34f8a12d4
Bryan Maupin <bmaupincode@gmail.com>
2011-12-08 15:06:07 -0600

    replace tabs with spaces

--- android_arabic_flashcards/src/us/bmaupin/flashcards/arabic/data/CardProvider.java
+++ android_arabic_flashcards/src/us/bmaupin/flashcards/arabic/data/CardProvider.java
@@ -10,27 +10,27 @@ import android.net.Uri;
 import android.util.Log;
 
 public class CardProvider extends ContentProvider {
-	private static final String TAG = "CardProvider";
+    private static final String TAG = "CardProvider";
     // various constants
     public static final String AUTHORITY = 
-    		"us.bmaupin.flashcards.arabic.cardprovider";
+            "us.bmaupin.flashcards.arabic.cardprovider";
     private static final int CARD_ID_PATH_POSITION = 1;
     private static final int LIMIT_PATH_POSITION = 2;
     private static final String PATH_CARDS = "cards";
     private static final String PATH_LIMIT = "limit";
     public static final Uri CONTENT_URI_CARDS = Uri.parse("content://" + 
-    		AUTHORITY + "/" + PATH_CARDS);
+            AUTHORITY + "/" + PATH_CARDS);
     public static final Uri CONTENT_URI_CARDS_LIMIT = Uri.parse("content://" + 
-    		AUTHORITY + "/" + PATH_CARDS + "/" + PATH_LIMIT);
+            AUTHORITY + "/" + PATH_CARDS + "/" + PATH_LIMIT);
     // content provider data type constants
     private static final int CARDS = 1;
     private static final int CARD_ID = 2;
     private static final int CARDS_LIMIT = 3;
     // content provider mime type constants
     public static final String CONTENT_TYPE = 
-    		"vnd.android.cursor.dir/vnd.bmaupin.card";
+            "vnd.android.cursor.dir/vnd.bmaupin.card";
     public static final String CONTENT_ITEM_TYPE = 
-    		"vnd.android.cursor.item/vnd.bmaupin.card";
+            "vnd.android.cursor.item/vnd.bmaupin.card";
     
     private CardDatabaseHelper cardDbHelper;
 
@@ -45,7 +45,7 @@ public class CardProvider extends ContentProvider {
         sUriMatcher.addURI(AUTHORITY, PATH_CARDS, CARDS);
         sUriMatcher.addURI(AUTHORITY, PATH_CARDS + "/#", CARD_ID);
         sUriMatcher.addURI(AUTHORITY, PATH_CARDS + "/" + PATH_LIMIT + "/#", 
-        		CARDS_LIMIT);
+                CARDS_LIMIT);
     }
     
     @Override
@@ -68,23 +68,23 @@ public class CardProvider extends ContentProvider {
         
         switch (sUriMatcher.match(uri)) {
         case CARDS_LIMIT:
-        	limit = uri.getPathSegments().get(LIMIT_PATH_POSITION);
+            limit = uri.getPathSegments().get(LIMIT_PATH_POSITION);
         
         case CARDS:
-        	// if we're searching for cards by ahlan wa sahlan chapter
-        	if (selection.indexOf(CardDatabaseHelper.AWS_CHAPTERS_CHAPTER)
-        			!= -1) {
-        		/*
-        		 * this looks like:
-        		 * cards left join aws_chapters on cards._id = aws_chapters.card_id
-        		 */
-        		qb.setTables(CardDatabaseHelper.CARDS_TABLE + " LEFT " +
-        				"JOIN " + CardDatabaseHelper.AWS_CHAPTERS_TABLE + 
-        				" ON " + CardDatabaseHelper.CARDS_TABLE + "." + 
-        				CardDatabaseHelper._ID + " = " + 
-        				CardDatabaseHelper.AWS_CHAPTERS_TABLE + "." + 
-        				CardDatabaseHelper.AWS_CHAPTERS_CARD_ID);
-        	}
+            // if we're searching for cards by ahlan wa sahlan chapter
+            if (selection.indexOf(CardDatabaseHelper.AWS_CHAPTERS_CHAPTER)
+                    != -1) {
+                /*
+                 * this looks like:
+                 * cards left join aws_chapters on cards._id = aws_chapters.card_id
+                 */
+                qb.setTables(CardDatabaseHelper.CARDS_TABLE + " LEFT " +
+                        "JOIN " + CardDatabaseHelper.AWS_CHAPTERS_TABLE + 
+                        " ON " + CardDatabaseHelper.CARDS_TABLE + "." + 
+                        CardDatabaseHelper._ID + " = " + 
+                        CardDatabaseHelper.AWS_CHAPTERS_TABLE + "." + 
+                        CardDatabaseHelper.AWS_CHAPTERS_CARD_ID);
+            }
             break;
         
         case CARD_ID:
@@ -107,14 +107,14 @@ public class CardProvider extends ContentProvider {
                 null,           // don't group the rows
                 null,           // don't filter by row groups
                 sortOrder,      // The sort order
-                limit          // limit 
+                limit           // limit 
             );
 
         // Tells the Cursor what URI to watch, so it knows when its source data changes
         c.setNotificationUri(getContext().getContentResolver(), uri);
         return c;
     }
-	
+    
     @Override
     public Uri insert(Uri uri, ContentValues values) {
         // not going to implement
@@ -123,18 +123,18 @@ public class CardProvider extends ContentProvider {
     
     @Override
     public int update(Uri uri, ContentValues values, String selection, 
-    		String[] selectionArgs) {
+            String[] selectionArgs) {
         // not going to implement
         return 0;
     }
-	
-	@Override
-	public int delete(Uri uri, String selection, String[] selectionArgs) {
+    
+    @Override
+    public int delete(Uri uri, String selection, String[] selectionArgs) {
         // not going to implement
-		return 0;
-	}
+        return 0;
+    }
 
-	@Override
+    @Override
     public String getType(Uri uri) {
        switch (sUriMatcher.match(uri)) {
 
