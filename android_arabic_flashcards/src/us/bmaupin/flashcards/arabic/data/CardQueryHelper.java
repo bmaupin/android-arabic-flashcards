ccc80f310dd42707f840cf246a0787f6a4439bed
bmaupin <bmaupin@users.noreply.github.com>
2012-01-25 13:56:38 -0600

    create card query helper class to try to eliminate/prevent some code redundancy

---
+++ android_arabic_flashcards/src/us/bmaupin/flashcards/arabic/data/CardQueryHelper.java
@@ -0,0 +1,63 @@
+package us.bmaupin.flashcards.arabic.data;
+
+import android.content.Context;
+import us.bmaupin.flashcards.arabic.R;
+
+class CardQueryHelper {
+    private String selection;
+    private String[] selectionArgs;
+    private String sortOrder;
+    
+    String getSelection() {
+        return selection;
+    }
+
+    String[] getSelectionArgs() {
+        return selectionArgs;
+    }
+
+    String getSortOrder() {
+        return sortOrder;
+    }
+
+    CardQueryHelper(Context context, String cardGroup, String cardSubgroup) {
+        if (cardGroup.equals(context.getString(
+                R.string.card_group_ahlan_wa_sahlan))) {
+            /*
+             * this looks like:
+             * where cards._id in (select card_ID from aws_chapters where chapter = ?) and chapter = ?
+             * 
+             * it may seem terribly redundant, but it's much faster than:
+             * where chapter = ?
+             * 
+             * because it doesn't do the left join (in the provider class) 
+             * on both entire tables
+             * 
+             * the entire query ends up looking like this:
+             * select * from cards left join aws_chapters on cards._id = aws_chapters.card_id where cards._id in (select card_ID from aws_chapters where chapter = 4) and chapter = 4 order by aws_chapters._id;
+             * 
+             * instead of this:
+             * select * from cards left join aws_chapters on cards._id = aws_chapters.card_id where chapter = 4 order by aws_chapters._id;
+             */
+            selection = CardDatabaseHelper.CARDS_TABLE + "." + 
+                    CardDatabaseHelper._ID + " IN (SELECT " + 
+                    CardDatabaseHelper.AWS_CHAPTERS_CARD_ID + " FROM " + 
+                    CardDatabaseHelper.AWS_CHAPTERS_TABLE + " WHERE " + 
+                    CardDatabaseHelper.AWS_CHAPTERS_CHAPTER + " = ?) AND " + 
+                    CardDatabaseHelper.AWS_CHAPTERS_CHAPTER + " = ? ";
+            selectionArgs = new String[] {cardSubgroup, cardSubgroup};
+            sortOrder = CardDatabaseHelper.AWS_CHAPTERS_TABLE + "." + 
+                    CardDatabaseHelper._ID;
+            
+        } else if (cardGroup.equals(context.getString(
+                R.string.card_group_categories))) {
+            selection = CardDatabaseHelper.CARDS_CATEGORY + " = ? ";
+            selectionArgs = new String[] {cardSubgroup};
+            
+        } else if (cardGroup.equals(context.getString(
+                R.string.card_group_parts_of_speech))) {
+            selection = CardDatabaseHelper.CARDS_TYPE + " = ? ";
+            selectionArgs = new String[] {cardSubgroup};
+        }
+    }
+}
