0d268b7270daeb2c7e91f52f45e9c94631d9ce11
b <bmaupincode@gmail.com>
2012-02-01 14:40:11 -0600

    Revert "add back method from commit e12ab for getting initial study set count"
    This reverts commit 393236c36a0527d23de0cdbda00a3663a5ed4375.

--- android_arabic_flashcards/src/us/bmaupin/flashcards/arabic/data/StudySetHelper.java
+++ android_arabic_flashcards/src/us/bmaupin/flashcards/arabic/data/StudySetHelper.java
@@ -45,53 +45,6 @@ public class StudySetHelper {
         return initialStudySetCount;
     }
     
-    public static int getInitialStudySetCount(Context context, int studySetId, 
-            int studySetCount) {
-        int initialStudySetCount = 0;
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(
-                StudySetDatabaseHelper.META_NEW_CARDS_DATE_FORMAT);
-        
-        // get initial count of cards in study set
-        Cursor cursor = context.getContentResolver().query(
-                StudySetProvider.CONTENT_URI_META,
-                new String[] {
-                        StudySetDatabaseHelper.META_INITIAL_COUNT_DATE,
-                        StudySetDatabaseHelper.META_INITIAL_COUNT},
-                StudySetDatabaseHelper._ID + " = ? ",
-                new String[] {"" + studySetId},
-                null);
-        
-        if (cursor.moveToFirst()) {
-            String today = simpleDateFormat.format(new Date()).toString();
-            
-            // if the date found in the database is today
-            if (cursor.getString(0).equals(today)) {
-                // set the initial count of cards in the study set for today
-                initialStudySetCount = cursor.getInt(1);
-                Log.d(TAG, "initialStudySetCount (from db)=" + initialStudySetCount);
-                
-            // otherwise update the initial study set card count in the db
-            } else {
-                ContentValues cv = new ContentValues();
-                cv.put(StudySetDatabaseHelper.META_INITIAL_COUNT_DATE, 
-                        today);
-                cv.put(StudySetDatabaseHelper.META_INITIAL_COUNT, 
-                        studySetCount);
-                
-                context.getContentResolver().update(
-                        StudySetProvider.CONTENT_URI_META,
-                        cv,
-                        StudySetDatabaseHelper._ID + " = ? ",
-                        new String[] {"" + studySetId});
-                initialStudySetCount = studySetCount;
-                Log.d(TAG, "initialStudySetCount=studySetCount=" + initialStudySetCount);
-            }
-        }
-        cursor.close();
-        
-        return initialStudySetCount;
-    }
-    
     /*
      * get the total count of rows in the study set table, to be used as an
      * offset for new cards
