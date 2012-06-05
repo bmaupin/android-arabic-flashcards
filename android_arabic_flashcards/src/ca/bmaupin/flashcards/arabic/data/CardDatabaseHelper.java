001ccc137072a2a6bbe97194b86764071586d1ac
bmaupin <bmaupin@users.noreply.github.com>
2012-06-05 14:00:32 -0500

    update plurals in database

--- android_arabic_flashcards/src/ca/bmaupin/flashcards/arabic/data/CardDatabaseHelper.java
+++ android_arabic_flashcards/src/ca/bmaupin/flashcards/arabic/data/CardDatabaseHelper.java
@@ -17,7 +17,7 @@ public class CardDatabaseHelper extends SQLiteOpenHelper {
 	private static final String TAG = "DatabaseHelper";
 	
 	public static final String DATABASE_NAME = "cards.db";
-	public static final int DATABASE_VERSION = 15;
+	public static final int DATABASE_VERSION = 16;
 	public static final String CARDS_TABLE = "cards";
 	public static final String AWS_CHAPTERS_TABLE = "aws_chapters";
 
