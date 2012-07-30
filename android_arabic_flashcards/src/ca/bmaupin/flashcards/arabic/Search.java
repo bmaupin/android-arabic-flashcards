7a7a24e4a4803f289a3b58702e7f33fee5f699eb
b <bmaupincode@gmail.com>
2012-07-30 15:06:59 -0500

    search mostly seems to be working like we want it.  still needs more testing

--- android_arabic_flashcards/src/ca/bmaupin/flashcards/arabic/Search.java
+++ android_arabic_flashcards/src/ca/bmaupin/flashcards/arabic/Search.java
@@ -25,6 +25,7 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class Search extends FragmentActivity 
         implements LoaderManager.LoaderCallbacks<Cursor>{
@@ -41,7 +42,7 @@ public class Search extends FragmentActivity
     // whether or not to apply arabic fixes
     private boolean fixArabic;
     private String[] from = new String[] {};
-    private Intent intent;
+//    private Intent intent;
     private ListView lv;
     private SharedPreferences preferences;
     private Resources resources;
@@ -68,7 +69,13 @@ public class Search extends FragmentActivity
         preferences = PreferenceManager.getDefaultSharedPreferences(this);
         resources = getResources();
         
-        this.intent = getIntent();
+        // get initial values of preferences
+        fixArabic = preferences.getBoolean(
+                getString(R.string.preferences_fix_arabic),
+                resources.getBoolean(R.bool.preferences_fix_arabic_default));
+        showVowels = preferences.getBoolean(
+                getString(R.string.preferences_show_vowels),
+                resources.getBoolean(R.bool.preferences_show_vowels_default));
         
         lv = (ListView) findViewById(android.R.id.list);
         
@@ -84,51 +91,8 @@ public class Search extends FragmentActivity
             }
         });
         
-        // create the adapter here so we can make arabic or english the primary 
-        // field for the cursor depending on the query
-        adapter = new MySimpleCursorAdapter(
-                this,
-                android.R.layout.simple_list_item_2,
-                null,
-                new String[] {CardDatabaseHelper.CARDS_ENGLISH, 
-                        CardDatabaseHelper.CARDS_ARABIC},
-                new int[] {android.R.id.text1, android.R.id.text2},
-                0,
-                fixArabic);
-
-        // http://stackoverflow.com/questions/3609126/changing-values-from-cursor-using-simplecursoradapter
-        adapter.setViewBinder(new ViewBinder() {
-            public boolean setViewValue(View aView, Cursor aCursor, int aColumnIndex) {
-                if (aColumnIndex == 2) {
-                    String arabic = aCursor.getString(aColumnIndex);
-                    TextView tv = (TextView) aView;
-                    if (fixArabic) {
-                        arabic = Cards.fixArabic(arabic, showVowels);
-                    }
-                    if (showVowels) {
-                        tv.setText(arabic);
-                    } else {
-                        tv.setText(Cards.removeVowels(arabic));
-                    }
-                    return true;
-                }
-                
-                return false;
-            }
-        });
-        
-        // Bind to our new adapter.
-        lv.setAdapter(adapter);
-        
-        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
-            getSupportLoaderManager().initLoader(0, null, this);
-            
-        } else if (Intent.ACTION_VIEW.equals(intent.getAction())) {
-            int cardId = Cards.stringToInteger(intent.getDataString());
-            Intent intent = new Intent(this, ShowOneCard.class);
-            intent.putExtra(EXTRA_CARD_ID, cardId);
-            startActivity(intent);
-        }
+        createAdapter();
+        handleIntent(getIntent());
     }
     
     @Override
@@ -136,8 +100,11 @@ public class Search extends FragmentActivity
         super.onNewIntent(intent);
         Log.d(TAG, "onNewIntent()");
         
-        this.intent = intent;
-        
+        setIntent(intent);
+        handleIntent(intent);
+    }
+
+    private void handleIntent(Intent intent) {
         if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
             getSupportLoaderManager().restartLoader(0, null, this);
             
@@ -148,18 +115,53 @@ public class Search extends FragmentActivity
             startActivity(intent);
         }
     }
-
+    
     @Override
     protected void onResume() {
         super.onResume();
         Log.d(TAG, "onResume()");
+
+//
+        if (Intent.ACTION_SEARCH.equals(getIntent().getAction())) {
+            Log.d(TAG, "search intent");
+            
+        } else if (Intent.ACTION_VIEW.equals(getIntent().getAction())) {
+            Log.d(TAG, "view intent");
+        }
+//
+        Log.d(TAG, "query=" + getIntent().getStringExtra(SearchManager.QUERY));
         
-        fixArabic = preferences.getBoolean(
+        // if there's no query (e.g. we're returning to search from showing a 
+        // search suggestion)
+        if (getIntent().getStringExtra(SearchManager.QUERY) == null) {
+            // finish the activity
+            finish();
+        }
+        
+        // if any of the preferences changed
+        if (preferences.getBoolean(
                 getString(R.string.preferences_fix_arabic),
-                resources.getBoolean(R.bool.preferences_fix_arabic_default));
-        showVowels = preferences.getBoolean(
+                resources.getBoolean(R.bool.preferences_fix_arabic_default)) !=
+                fixArabic || preferences.getBoolean(
                 getString(R.string.preferences_show_vowels),
-                resources.getBoolean(R.bool.preferences_show_vowels_default));
+                resources.getBoolean(R.bool.preferences_show_vowels_default)) 
+                != showVowels) {
+//            
+            Toast.makeText(getApplicationContext(), "preferences changed", Toast.LENGTH_SHORT).show();
+            
+            // update them
+            fixArabic = preferences.getBoolean(
+                    getString(R.string.preferences_fix_arabic),
+                    resources.getBoolean(R.bool.preferences_fix_arabic_default));
+            showVowels = preferences.getBoolean(
+                    getString(R.string.preferences_show_vowels),
+                    resources.getBoolean(R.bool.preferences_show_vowels_default));
+
+            // recreate the adapter to reflect changes
+            createAdapter();
+
+            getSupportLoaderManager().restartLoader(0, null, this);
+        }
     }
     
     @Override
@@ -170,7 +172,7 @@ public class Search extends FragmentActivity
         String selection = "";
         String[] selectionArgs = new String[] {};
         
-        String query = intent.getStringExtra(SearchManager.QUERY);
+        String query = getIntent().getStringExtra(SearchManager.QUERY);
         
         // if the query is arabic
         if (ArabicUtilities.isArabicWord(query)) {
@@ -209,6 +211,7 @@ public class Search extends FragmentActivity
         // old cursor once we return.)
         adapter.swapCursor(data);
 // TODO swap adapter columns
+// http://developer.android.com/reference/android/widget/SimpleCursorAdapter.html#changeCursorAndColumns(android.database.Cursor, java.lang.String[], int[])
     }
 
     @Override
@@ -218,6 +221,45 @@ public class Search extends FragmentActivity
         // above is about to be closed.  We need to make sure we are no
         // longer using it.
         adapter.swapCursor(null);
+    }
+    
+    private void createAdapter() {
+        // create the adapter here so we can make arabic or english the primary 
+        // field for the cursor depending on the query
+        adapter = new MySimpleCursorAdapter(
+                this,
+                android.R.layout.simple_list_item_2,
+                null,
+                new String[] {CardDatabaseHelper.CARDS_ENGLISH, 
+                        CardDatabaseHelper.CARDS_ARABIC},
+                new int[] {android.R.id.text1, android.R.id.text2},
+                0,
+                fixArabic);
+
+        // http://stackoverflow.com/questions/3609126/changing-values-from-cursor-using-simplecursoradapter
+        adapter.setViewBinder(new ViewBinder() {
+            public boolean setViewValue(View aView, Cursor aCursor, int aColumnIndex) {
+// TODO: aColumnIndex == 2 may not work if we swap the arabic and english columns
+                if (aColumnIndex == 2) {
+                    String arabic = aCursor.getString(aColumnIndex);
+                    TextView tv = (TextView) aView;
+                    if (fixArabic) {
+                        arabic = Cards.fixArabic(arabic, showVowels);
+                    }
+                    if (showVowels) {
+                        tv.setText(arabic);
+                    } else {
+                        tv.setText(Cards.removeVowels(arabic));
+                    }
+                    return true;
+                }
+                
+                return false;
+            }
+        });
+        
+        // Bind to our new adapter.
+        lv.setAdapter(adapter);
     }
 
     @Override
@@ -252,7 +294,7 @@ class MySimpleCursorAdapter extends SimpleCursorAdapter {
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         View v = super.getView(position, convertView, parent);
-
+// TODO: android.R.id.text2 may not work if we swap the arabic and english columns
         if (fixArabic) {
             ((TextView) v.findViewById(android.R.id.text2)).setTypeface(
                     Typeface.createFromAsset(context.getAssets(), 
